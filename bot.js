2fde91efc1aaab20afd4bb18d877f74ca1126a09
Albert <Albert.Y.Luppol@gmail.com>
2020-09-17 19:57:52 +0300

    init fifth

--- bot.js
+++ bot.js
@@ -6,3 +6,12 @@ http
     res.end("It works fine!");
   })
   .listen(process.env.PORT || 8080);
+
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf("1205181243:AAHQjMdn8dFbJzoeWQFppVd-irqmwEpTuqo");
+bot.start((ctx) => ctx.reply("Welcome!"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on("sticker", (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.launch();
